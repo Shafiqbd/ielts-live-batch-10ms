77dfe31f2ed675e59dd269e7d51c238dada062a6
Shafiqbd <shafiq523000@gmail.com>
2024-12-07 13:05:33 +0600

    endpoint change

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -1,2 +1,3 @@
 export const apiUrl =
-  process.env.NEXT_PUBLIC_API_URL || "https://api.allroundbd.com/allround";
+  process.env.NEXT_PUBLIC_API_URL ||
+  "https://api.10minuteschool.com/discovery-service/api/v1";
